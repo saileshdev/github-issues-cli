49872cbed9d7a9216c5c1a7bf1506349ebb70e51
Sailesh Dev <saileshdev@gmail.com>
2016-03-03 13:55:32 +0530

    Adds github api url in the config.ex file

--- config/config.exs
+++ config/config.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # You can configure for your application as:
 #
 #     config :issues, key: :value
+config :issues, github_url: "https://api.github.com"
 #
 # And access this configuration in your application as:
 #
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,19 +1,26 @@
+
 defmodule Issues.GithubIssues do
+ 
+ @github_url Application.get_env(:issues, :github_url)
+      
+  @user_agent [{"User-agent", "saileshdev@gmail.com"}]
 
-  @user_agent [{"User-agent","saileshdev@gmail.com"}]
+  def fetch(user, project) do
+     issues_url(user, project)
+      |> HTTPoison.get(@user_agent)
+      |> handle_response
+      end
 
-  def fetch(user,project) do
-    issues_url(user,project)
-    |> HTTPoison.get(@user_agent)
-    |> handle_response
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def issues_url(user,project) do 
-    "http://api.github.com/repos/#{user}/#{project}/issues"
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+  {:ok, body}
   end
 
-  def handle_response(%{status_code: 200, body: body}), do: {:ok, body}
-
-  def handle_response(%{status_code:_, body: body}), do: {:error, body}
+  def handle_response({:error, %{status_code: status, body: body}}) do
+  {:error, body}
+  end
 
 end
